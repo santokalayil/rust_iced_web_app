8be04ff2dce313eb0384a72f0b5c3d4917753edc
ezetap <santokalayil@gmail.com>
2022-10-16 02:28:07 +0530

    Some issues. Iced documentation does not help

--- src/data.rs
+++ src/data.rs
@@ -18,3 +18,44 @@ pub struct Comment {
     pub email: String,
     pub body: String,
 }
+
+
+impl Post {
+    pub async fn fetch_all() -> Result<Vec<Post>, String> {
+        let url = String::from("https://jsonplaceholder.typicode.com/posts/");
+        reqwest::get(&url)
+            .await
+            .map_err(|_| String::new())?
+            .json()
+            .await
+            .map_err(|_| String::new())
+    }
+
+    pub async fn fetch(id: i32) -> Result<Post, String> {
+        let url = format!("https://jsonplaceholder.typicode.com/posts/{}", id);
+        reqwest::get(&url)
+            .await
+            .map_err(|_| String::new())?
+            .json()
+            .await
+            .map_err(|_| String::new())
+    }
+}
+
+
+impl Comment {
+    pub async fn fetch_for_post(id: i32) -> Result<Vec<Comment>, String> {
+        let url = format!(
+            "https://jsonplaceholder.typicode.com/posts/{}/comments/",
+            id
+        );
+        reqwest::get(&url)
+            .await
+            .map_err(|_| String::new())?
+            .json()
+            .await
+            .map_err(|_| String::new())
+    }
+}
+
+
